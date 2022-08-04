3d9a854af1dca9d31f4e635c431538c30ab09754
Mohamed Allam <mohamedallam.tu@gmail.com>
2022-08-04 19:36:59 +0200

    Update DBHandler

--- APIs.js
+++ APIs.js
@@ -2,12 +2,63 @@
   root.API = factory()
 })(this, function () {
 
+  const { addToDB, updateInDB, lookupDB } = JSON_DB_HANDLER;
+  const CCONE = "CCOne";
+
   function handleRequest(request, dbName) {
+    const { userId } = getUserId(request);
+    const { applicationId } = getApplicationId(request, dbName);
+    const { fillCheck, roles } = request
+    if (!applicationId) {
+      addNewApplicationToDB(request, dbName);
+    } else {
+      updateApplicationInDB(request, dbName);
+    }
+    if (!userId) {
+      if (applicationObj.fillCheck) {
+        userId = getId("CCER", row.Timestamp);
+        addNewUserToDB(userId, applicationId, row);
+      } else {
+        warn(applicationObj.Email);
+      }
+    } else {
+      if (roles.includes("Applicant") && !fillCheck) {
+        warn(applicationObj.Email);
+      } else {
+        updateUserInDB(userId, applicationId, row, fillCheck);
+      }
+    }
+
     addNewEntry(request, dbName)
   }
 
-  function addNewEntry(request, dbName) {
-    JSON_DB_HANDLER.addToDB(request, dbName)
+
+  function getApplicationId(request, dbName) {
+    lookupDB(request, dbName)
+  }
+
+  function getUserId(request) {
+    lookupDB(request, CCONE)
+  }
+
+  function addNewApplicationToDB(request, dbName) {
+    addToDB(request, dbName)
+  }
+
+  function addNewUserToDB(request) {
+    addToDB(request, CCONE)
+  }
+
+  function updateApplicationInDB(request, dbName) {
+    updateInDB(request, dbName)
+  }
+
+  function updateUserInDB() {
+    updateInDB(request, CCONE)
+  }
+
+  function warn() {
+
   }
 
   return {
--- DBHandler.js
+++ DBHandler.js
@@ -1,151 +1,179 @@
-/// Data Flows in
-/// Save to DB in 2 steps:
-/// Save to Local JS Object - Based on Secondary classifier and what type of DB is it? Main data / statuses / complementary
-/// Object write to File in fragmentation
-
-/// Check if existing in file: check with if query array includes key
-
-
 ; (function (root, factory) {
   root.JSON_DB_HANDLER = factory()
 })(this, function () {
 
-  const { Toolkit } = CCLIBRARIES;
+  const { Toolkit } = CCLIBRARIES
 
+  const MAX_ENTRIES_COUNT = 10000;
 
-  let indexFileObj;
-  const openDBs = {};
+  let INDEX = {
+      CCONE: {
+          properties: {
+              rootFolder: "",
+              filesPrefix: ""
+          },
+          dbFragments: {
+              CCONE_1: {
+                  queryArray: [],
+                  fileId: ""
+              }
+          }
+      },
+      CCG: {
+          properties: {
+              rootFolder: "",
+              filesPrefix: ""
+          },
+          dbFragments: {
+              CCGS1R1: {
+                  queryArray: [],
+                  fileId: ""
+              },
+              CCGS1R2: {
+                  queryArray: [],
+                  fileId: ""
+              }
+          }
+      }
+  }
+
+  let OPEN_DB = {
+      CCONE_1: {
+          properties: {
+              isChanged: true
+          },
+          toWrite: {
+              index: {},
+              data: {}
+          }
+
+      },
+      CCGS1R1: {
+          propertties: {
+              isChanged: true
+          },
+          toWrite: {
+              index: {},
+              data: {}
+          }
+
+      }
+  }
 
   function initiateDB(indexFileId) {
-    indexFileObj = Toolkit.readFromJSON(indexFileId);
+      INDEX = Toolkit.readFromJSON(indexFileId);
   }
 
-  function addToDB(request, dbName) {
-    checkDB(dbName);
-    const { id, identifier } = request;
-    openDBs[dbName].toWrite.index[identifier] = id;
-    openDBs[dbName].toWrite.data[id] = request;
+  function saveAndClose() {
 
   }
 
-  function lookupDB(key, dbName) {
-    checkDB(dbName);
+  function addToDB(entry, { dbMain, dbFragment }) {
+      CheckIfInIndexAndIfInOpen(dbMain, dbFragment)
+      if (dbMain && !dbFragment) dbFragment = getLastCreatedFragment(dbMain);
+      dbFragment = checkOpenDBSize(dbMain, dbFragment);
+      const { key, id } = entry;
+      OPEN_DB[dbFragment].toWrite.index[key] = id;
+      OPEN_DB[dbFragment].toWrite.data[id] = entry;
   }
 
-  function updateInDB(entry, dbName) {
+  function updateInDB(entry, { dbMain, dbFragment }) {
+      OPEN_DB[dbFragment].toWrite.index[key] = id;
+      OPEN_DB[dbFragment].toWrite.data[id] = entry;
+  }
+
+  function lookUp(key, { dbMain, dbFragment }) {
+      CheckIfInIndexAndIfInOpen(dbMain, dbFragment)
+      if (dbMain) return lookUpByQueryArray(key, dbMain);
+      return lookUpInFragment(key, dbFragment);
+  }
+
+  function lookUpByQueryArray(key, dbMain) {
+      const { dbFragments } = INDEX[dbMain];
+      let entry;
+      Object.keys(dbFragments).forEach(dbFragment => {
+          const { queryArray, fileId } = dbFragments[dbFragment];
+          if (!queryArray.includes(key)) return;
+          openDBFragment(dbFragment, fileId);
+          entry = lookUpInFragment(key, dbFragment)
+      })
+      return entry
+  }
+
+  function lookUpInFragment(key, dbFragment) {
+      const { toWrite } = OPEN_DB[dbFragment];
+      const id = toWrite.index[key];
+      const entry = toWrite.data[id];
+      return entry;
+  }
+
+  function CheckIfInIndexAndIfInOpen() {
 
   }
 
-  function checkDB(dbName) {
-    if (!indexFileObj[dbName]) initiateIndex(dbName);
-    if (!openDBs[dbName]) {
-      if (!indexFileObj[dbName]) openDBs[dbName] = new OpenDBEntry();
-      else openDBs[dbName] = getDBFileFromIndex(dbName);
-    }
+  function openDBFragment(dbFragment, fileId) {
+      if (OPEN_DB[dbFragment]) return;
+      const fragmentFileObj = Toolkit.readFromJSON(fileId);
+      addToOpenDBsObj(dbFragment, fragmentFileObj)
   }
 
-  function getDBFileFromIndex(dbName) {
-    const { db } = indexFileObj[dbName];
-    openDBs[dbName].toWrite = {};
-    db.forEach(dbFileIndexEntry => {
-      const { fileId } = dbFileIndexEntry;
-      if (!fileId) return;
-      const dbFileContent = Toolkit.readFromJSON(fileId);
-      openDBs[dbName].toWrite = { ...openDBs[dbName].toWrite, ...dbFileContent };
-    })
+  function addToOpenDBsObj(dbFragment, fragmentFileObj) {
+      OPEN_DB[dbFragment] = new OpenDBEntry(fragmentFileObj)
+  }
+
+  function checkOpenDBSize(dbMain, dbFragment) {
+      const { toWrite } = OPEN_DB[dbFragment];
+      const { data } = toWrite;
+      if (data.length >= MAX_ENTRIES_COUNT) return createNewFragment(dbMain);
+      return dbFragment;
+  }
+
+  function createNewFragment(dbMain) {
+      const lastDBFragment = getLastCreatedFragment(dbMain);
+      const countingRegex = /_\d/g
+      if (countingRegex.test(lastDBFragment)) {
+          const count = parseInt(paragraph.match(countingRegex)[1]);
+          const newFragment = lastDBFragment.replace(countingRegex, "") + "_" + count++;
+      } else {
+          const newFragment = lastDBFragment + "_2";
+      }
+      INDEX[dbMain].dbFragments[newFragment] = new IndexEntry();
+  }
+
+  function getLastCreatedFragment(dbMain) {
+      const { dbFragments } = INDEX[dbMain];
+      const dbFragmentArray = Object.keys(dbFragments);
+      const fragmentsCount = dbFragmentArray.length;
+      return dbFragmentArray[fragmentsCount - 1]
+  }
+
+  function OpenDBEntry(fragmentFileObj) {
+      this.properties = new OpenDBProperties();
+      this.toWrite = fragmentFileObj || new DBFileObj();
 
   }
 
-  function OpenDBEntry() {
-    this.toWrite = new DBFileObj();
-
-
+  function OpenDBProperties() {
+      this.isChanged = true
   }
 
   function DBFileObj() {
-    this.index = {}
-    this.data = {}
+      this.index = {}
+      this.data = {}
   }
 
-  function IndexFile(rootFolder) {
-    this.properties = new PropertiesObj(rootFolder, prefix);
-    this.db = [new DBIndexEntryObj(queryArray, id)];
+  function IndexEntry() {
+      this.queryArray = [];
+      this.fileId = "";
   }
-
-  function PropertiesObj(rootFolder, prefix) {
-    return {
-      rootFolder,
-      prefix
-    }
-  }
-
-  function DBIndexEntryObj(queryArray, id) {
-    this.queryArray = [];
-    this.fileId = fileId
-  }
-
-  /// Methods:
-
-
-  function checkInDB(key) {
-
-  }
-
-  // function addToDB() {
-
-  // }
-
-  function updateDB() {
-
-  }
-
-  function saveCloseDB() {
-    if (fragment) fragmentDB()
-    dbFileObj
-  }
-
-  // Add to DB (by secondary classifier and DB name)
-  // Check existing
-  // Write to files
 
   return {
-    initiateDB,
-    addToDB
+      initiateDB,
+      addToDB,
+      updateInDB,
+      lookUp,
+      saveAndClose
   }
 
 })
 
-const INDEX = {
-  Confessions: {
-    properties: {
-      rootFolder: "",
-      prefix: {
-
-      }
-    },
-    db: {
-      mainData: {
-        file1: {
-          queryArray: [],
-          id: ""
-        }
-      },
-
-    }
-  },
-  Events: {
-
-  },
-  CCOne: {
-
-  },
-  Activities: {
-
-  }
-}
-
-
-
-function myFunction() {
-
-}
